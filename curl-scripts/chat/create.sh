4187c653b4c9a2ad7e4d6d895be79dd7a50910d2
jackson916 <colingrey412@gmail.com>
2022-05-02 10:49:37 -0400

    testing curl-scripts

--- curl-scripts/chat/create.sh
+++ curl-scripts/chat/create.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
+# NAME="Colin"
+# MESSAGE="THIS IS MY CHAT APP"
+# TOKEN="a4399f70f0420dc600cc071aad33e2af"
 
 API="http://localhost:4741"
-URL_PATH="/examples"
+URL_PATH="/chat"
 
 curl "${API}${URL_PATH}" \
   --include \
@@ -9,9 +12,9 @@ curl "${API}${URL_PATH}" \
   --header "Content-Type: application/json" \
   --header "Authorization: Bearer ${TOKEN}" \
   --data '{
-    "example": {
-      "text": "'"${TEXT}"'",
-      "title": "'"${TITLE}"'"
+    "chat": {
+      "name": "'"${NAME}"'",
+      "message": "'"${MESSAGE}"'"
     }
   }'
 
