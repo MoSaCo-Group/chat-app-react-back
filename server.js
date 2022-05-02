65366562bf693b07712bc99a6f0af31995a79ec5
ms00l <moesuleiman8@gmail.com>
2022-05-02 10:21:51 -0400

    groupdevelopment inital commit Co-authored-by: Colin Jackson colingrey412@gmail.com Co-authored-by: Sako Pak sako.taya@gmail.com

--- server.js
+++ server.js
@@ -34,10 +34,32 @@ mongoose.connect(db, {
 
 // instantiate express application object
 const app = express()
+const http = require('http')
 
+const server = http.createServer(app)
+const { Server } = require('socket.io')
+
+const io = new Server(server)
+
+io.on('connection', (socket) => {
+  socket.on('chat message', (msg) => {
+    console.log('message: ' + msg)
+  })
+})
+
+io.on('connection', (socket) => {
+  console.log('a user connected')
+  socket.on('disconnect', () => {
+    console.log('user disconnected')
+  })
+})
 // set CORS headers on response from this API using the `cors` NPM package
 // `CLIENT_ORIGIN` is an environment variable that will be set on Heroku
-app.use(cors({ origin: process.env.CLIENT_ORIGIN || `http://localhost:${clientDevPort}` }))
+app.use(
+  cors({
+    origin: process.env.CLIENT_ORIGIN || `http://localhost:${clientDevPort}`
+  })
+)
 
 // define port for API to run on
 const port = process.env.PORT || serverDevPort
