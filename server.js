91877fdb84b8adcef56baf720f3f560bef7c06d7
SakoPak <sako.taya@gmail.com>
2022-05-06 10:30:39 -0700

    Adding to get ready to pull

--- server.js
+++ server.js
@@ -36,16 +36,15 @@ mongoose.connect(db, {
 // instantiate express application object
 const app = express()
 const http = require('http')
-const socket = io.connect({ transports: ['websocket'] })
+
 const server = http.createServer(app)
 const { Server } = require('socket.io')
 
 const io = new Server(server)
 
 io.on('connection', (socket) => {
-  console.log('a user connected')
-  socket.on('disconnect', () => {
-    console.log('user disconnected')
+  socket.on('chat message', (msg) => {
+    console.log('message: ' + msg)
   })
 })
 
@@ -55,7 +54,6 @@ io.on('connection', (socket) => {
     console.log('user disconnected')
   })
 })
-
 // set CORS headers on response from this API using the `cors` NPM package
 // `CLIENT_ORIGIN` is an environment variable that will be set on Heroku
 app.use(
