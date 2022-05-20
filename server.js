cf9df60e17ee1fc4cea3597822ae8175ad75e759
SakoPak <sako.taya@gmail.com>
2022-05-20 14:46:18 -0700

    new url

--- server.js
+++ server.js
@@ -42,7 +42,7 @@ const socketIo = require('socket.io')
 // server is allowing requests from client
 const io = socketIo(server, {
   cors: {
-    origin: 'http://localhost:4741',
+    origin: 'https://mosaco-group.github.io',
     methods: ['GET', 'POST']
   }
 })
